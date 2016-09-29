36a776f9d2761d035174ee55717aa96c30bc2d02
Satit Rianpit <rianpit@gmail.com>
2016-09-29 23:55:01 +0700

    fixed IUser interface

--- src/pages/detail/detail.ts
+++ src/pages/detail/detail.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
+export interface IUser {
+  name: string,
+  email: string
+}
+
 @Component({
   selector: 'page-detail',
   templateUrl: 'detail.html'
 })
 export class DetailPage {
 
-  user: Object
+  user: IUser
 
   constructor(public navCtrl: NavController, private navParams: NavParams) {}
 
